3f9848590fd6bf5365f660cdd72f716fb51536ce
Joah Quevedo Klein Haneveld <joahmqkh@gmail.com>
2026-06-17 14:28:26 +0200

    FIXED wall tiles, table spawns

--- src/js/background.js
+++ src/js/background.js
@@ -18,18 +18,20 @@ export class Background extends Actor {
         for (let tile of tilemap.tiles) {
             // tile.addGraphic(spriteSheet.getSprite(2, 2));
             const columns = tilemap.columns
-            if (index < columns) {  //upperwal
+            const tileCount = tilemap.tiles.length
+            if (index < columns && index !== 0 && index !== columns - 1) {  //upperwal
                 tile.addGraphic(cafWalls.getSprite(0, 0));
                 tile.solid = true
                 // console.log(tile)
             } else if (index % columns === 0) {     //left wall
                 tile.addGraphic(cafWalls.getSprite(2, 0));
-                tilemap.tiles[0].addGraphic(cafWalls.getSprite(2, 0))
+                tilemap.tiles[tileCount - columns].addGraphic(cafWalls.getSprite(4, 0))
                 tile.solid = true
             } else if (index % columns === columns - 1) {   //right wall
                 tile.addGraphic(cafWalls.getSprite(1, 0));
+                tilemap.tiles[tileCount-1].addGraphic(cafWalls.getSprite(3, 0))
                 tile.solid = true
-            } else if (index > tilemap.tiles.length - columns) {    //bottom wall
+            } else if (index > tileCount - columns) {    //bottom wall
                 tile.addGraphic(cafWalls.getSprite(0, 0));
                 tile.solid = true
             } else {
