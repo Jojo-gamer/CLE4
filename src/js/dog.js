fcaace8acdd7f3687f84729efa36ac2939e58b32
Joah Quevedo Klein Haneveld <joahmqkh@gmail.com>
2026-06-17 15:21:08 +0200

    ADD dog sprite & table fade out

--- src/js/dog.js
+++ src/js/dog.js
@@ -6,31 +6,46 @@ export class Dog extends Actor {
         super({
             width: 40,
             height: 50,
-            color: Color.Brown,
             pos: new Vector(0, 0),
+            scale: new Vector(0.6, 0.6)
         })
     }
 
     onInitialize(engine) {
         this.actions.follow(this.scene.player, 75)
         this.body.collisionType = CollisionType.Passive
+        this.graphics.use(Resources.DogFront.toSprite())
+        this.player = this.scene.player
+        this.dir = Vector.Right
     }
 
     onPreUpdate(engine) {
-        if (engine.input.keyboard.wasPressed(Keys.Space)) {
-            const player = this.scene.player
-            let dir = Vector.Up
-            if (player.dirUp) dir = Vector.Up
-            if (player.dirDown) dir = Vector.Down
-            if (player.dirLeft) dir = Vector.Left
-            if (player.dirRight) dir = Vector.Right
+        if (this.player.dirUp) {
+            this.dir = Vector.Up
+            this.graphics.use(Resources.DogBack.toSprite())
+        }
+        if (this.player.dirDown) {
+            this.dir = Vector.Down
+            this.graphics.use(Resources.DogFront.toSprite())
+        }
+        if (this.player.dirLeft) {
+            this.dir = Vector.Left
+            this.graphics.use(Resources.DogSide.toSprite())
+            this.graphics.flipHorizontal = true;
+        }
+        if (this.player.dirRight) {
+            this.dir = Vector.Right
+            this.graphics.use(Resources.DogSide.toSprite())
+            this.graphics.flipHorizontal = false;
+        }
 
-            const ray = new Ray(this.pos, dir);
+        if (engine.input.keyboard.wasPressed(Keys.Space)) {
+            const ray = new Ray(this.pos, this.dir);
             const hits = this.scene.physics.rayCast(ray, {
                 searchAllColliders: false, // Stop direct bij het eerste doelwit
                 maxDistance: 350,
                 filter: (hit) => {
-                    if (hit.collider.owner !== player && hit.collider.owner !== this) {
+                    if (hit.collider.owner !== this.player && hit.collider.owner !== this) {
                         return true
                     }
                 }
@@ -39,6 +54,7 @@ export class Dog extends Actor {
                 const owner = hits[0].collider.owner
                 if (!owner.isReal) {
                     owner.body.collisionType = CollisionType.Passive
+                    owner.actions.fade(0.3, 1000)
                 } else {
                     console.log('WOOF')
                     Resources.BarkSound.play()
