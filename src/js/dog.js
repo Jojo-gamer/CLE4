549f329392457672b4b10ca6ad13fcaa341cc25e
Nat <nathan12khoun@gmail.com>
2026-06-22 13:00:17 +0200

    dog sheets

--- src/js/dog.js
+++ src/js/dog.js
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Color, Keys, Ray, Sound, TileMap, Vector } from "excalibur";
+import { Actor, CollisionType, Color, Keys, Ray, Sound, SpriteSheet, TileMap, Vector, range, Animation } from "excalibur";
 import { Resources } from "./resources";
 import { Enemy } from "./enemy";
 
@@ -18,25 +18,69 @@ export class Dog extends Actor {
         this.graphics.use(Resources.DogFront.toSprite())
         this.player = this.scene.player
         this.dir = Vector.Right
+
+        //Importing sprite sheets
+        const dogUp = SpriteSheet.fromImageSource({
+            image: Resources.DogFront,
+            grid: {rows:1 , columns: 4, spriteWidth: 128, spriteHeight: 128,}
+        })
+
+        const dogSide = SpriteSheet.fromImageSource({
+            image: Resources.DogSide,
+            grid: {rows:1 , columns: 4, spriteWidth: 128, spriteHeight: 128,}
+        })
+
+        const dogDown = SpriteSheet.fromImageSource({
+            image: Resources.DogBack,
+            grid: {rows:1 , columns: 4, spriteWidth: 128, spriteHeight: 128,}
+        })
+
+        const movingUp = Animation.fromSpriteSheet(dogUp, range(0, 3), 75)
+        const movingSide = Animation.fromSpriteSheet(dogSide, range(0, 3), 75)
+        const movingDown = Animation.fromSpriteSheet(dogDown, range(0, 3), 75)
+
+        const idleUp = dogUp.getSprite(0,0)
+        const idleSide = dogSide.getSprite(0,0)
+        const idleDown = dogDown.getSprite(0,0)
+
+        this.graphics.add("movingUp", movingUp)
+        this.graphics.add("movingSide", movingSide)
+        this.graphics.add("movingDown", movingDown)
+
+        this.graphics.add("idleUp", idleUp)
+        this.graphics.add("idleSide", idleSide)
+        this.graphics.add("idleDown", idleDown)
+
+        this.movingUp = this.graphics.use(movingUp)
+        this.movingSide = this.graphics.use(movingSide)
+        this.movingDown = this.graphics.use(movingDown)
+
+        this.idleUp = this.graphics.use(idleUp)
+        this.idleSide = this.graphics.use(idleSide)
+        this.idleDown = this.graphics.use(idleDown)
     }
 
     onPreUpdate(engine) {
         if (this.player.dirUp) {
             this.dir = Vector.Up
-            this.graphics.use(Resources.DogBack.toSprite())
+            if (this.dir.y < 0) {
+                this.graphics.use(this.movingDown)
+            } else {
+                this.graphics.use(this.idleDown)
+            }
         }
         if (this.player.dirDown) {
             this.dir = Vector.Down
-            this.graphics.use(Resources.DogFront.toSprite())
+            this.graphics.use(this.movingUp)
         }
         if (this.player.dirLeft) {
             this.dir = Vector.Left
-            this.graphics.use(Resources.DogSide.toSprite())
+            this.graphics.use(this.movingSide)
             this.graphics.flipHorizontal = true;
         }
         if (this.player.dirRight) {
             this.dir = Vector.Right
-            this.graphics.use(Resources.DogSide.toSprite())
+            this.graphics.use(this.movingSide)
             this.graphics.flipHorizontal = false;
         }
 
@@ -62,7 +106,7 @@ export class Dog extends Actor {
 
                     //chance to spawn new enemy
                     if (Math.random() > 0.25) {
-                        console.log('spawn')
+                        // console.log('spawn')
 
                         const isReal = Math.random() > 0.50;
                         const enemy = new Enemy(isReal)
