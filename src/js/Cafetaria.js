c318106b79bcf6a27d7fa10886e4613724071ce1
Nat <nathan12khoun@gmail.com>
2026-06-23 10:22:28 +0200

    adjusted door spawnpoints in cafetaria eastwing

--- src/js/Cafetaria.js
+++ src/js/Cafetaria.js
@@ -47,7 +47,7 @@ export class Cafetaria extends Scene {
         this.camera.strategy.lockToActor(this.player)
         this.camera.strategy.limitCameraBounds(new BoundingBox(0, 0, this.sceneWidth, this.sceneHeight))
 
-        this.add(new DoorTrigger(130, 1000, 50, 150, "EastWing", 1300, 300, false));
+        this.add(new DoorTrigger(130, 1000, 50, 150, "EastWing", 2200, 310 , false));
 
         this.add(new DoorTrigger(1500, 140, 150, 50, "CourtYard", 1500, 1940, false));
     }
