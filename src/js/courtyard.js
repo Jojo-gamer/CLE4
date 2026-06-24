a0d772b425ace6df82f9f98a28c575f7cb422ec9
BHR den Boer <bramdb16703@gmail.com>
2026-06-24 15:03:09 +0200

    formatted code

--- src/js/courtyard.js
+++ src/js/courtyard.js
@@ -1,42 +1,58 @@
-import { Actor, Engine, Vector, DisplayMode, BoundingBox, Color, SolverStrategy, Timer, Scene, randomInRange } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
-import { Background } from "./background.js"
+import {
+  Actor,
+  Engine,
+  Vector,
+  DisplayMode,
+  BoundingBox,
+  Color,
+  SolverStrategy,
+  Timer,
+  Scene,
+  randomInRange,
+} from "excalibur";
+import { Resources, ResourceLoader } from "./resources.js";
+import { Background } from "./background.js";
 import { DoorTrigger } from "./doorTrigger.js";
 import { Player } from "./player.js";
-import { Dog } from './dog.js'
+import { Dog } from "./dog.js";
 import { Enemy } from "./enemy.js";
 
 export class CourtYard extends Scene {
-    constructor() {
-        const sceneWidth = 3000
-        const sceneHeight = 2000
-        super({
-            width: sceneWidth,
-            height: sceneHeight,
-        })
+  constructor() {
+    const sceneWidth = 3000;
+    const sceneHeight = 2000;
+    super({
+      width: sceneWidth,
+      height: sceneHeight,
+    });
 
-        this.sceneWidth = sceneWidth
-        this.sceneHeight = sceneHeight
-    }
-    onInitialize(engine) {
-        this.background = new Background(this.sceneWidth, this.sceneHeight, engine.currentSceneName)
-        this.add(this.background)
+    this.sceneWidth = sceneWidth;
+    this.sceneHeight = sceneHeight;
+  }
+  onInitialize(engine) {
+    this.background = new Background(
+      this.sceneWidth,
+      this.sceneHeight,
+      engine.currentSceneName,
+    );
+    this.add(this.background);
 
-        this.player = new Player();
-        const spawnPoint = this.engine.nextSpawn
-        this.player.pos = new Vector(spawnPoint.x, spawnPoint.y)
-        this.add(this.player);
+    this.player = new Player();
+    const spawnPoint = this.engine.nextSpawn;
+    this.player.pos = new Vector(spawnPoint.x, spawnPoint.y);
+    this.add(this.player);
 
-        this.dog = new Dog()
-        this.dog.pos = this.player.pos
-        this.add(this.dog)
+    this.dog = new Dog();
+    this.dog.pos = this.player.pos;
+    this.add(this.dog);
 
+    this.camera.strategy.lockToActor(this.player);
+    this.camera.strategy.limitCameraBounds(
+      new BoundingBox(0, 0, this.sceneWidth, this.sceneHeight),
+    );
 
-        this.camera.strategy.lockToActor(this.player)
-        this.camera.strategy.limitCameraBounds(new BoundingBox(0, 0, this.sceneWidth, this.sceneHeight))
-
-
-        this.add(new DoorTrigger(1500, 1860, 150, 50, "Cafetaria", 1500, 150, 'down'));
-
-    }
+    this.add(
+      new DoorTrigger(1500, 1860, 150, 50, "Cafetaria", 1500, 150, "down"),
+    );
+  }
 }
